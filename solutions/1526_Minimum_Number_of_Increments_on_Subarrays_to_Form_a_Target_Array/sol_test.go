b34abbb4df1ed2c227986b90272f44c58adc058e
edag94 <edag94@live.com>
2021-11-08 22:02:16 -0800

    1526 done

--- solutions/1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array/sol_test.go
+++ solutions/1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array/sol_test.go
@@ -2,8 +2,12 @@ package Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test(t *testing.T) {
-
+	var testArray []int = []int{3, 1, 5, 4, 2}
+	ans := minNumberOperations(testArray)
+	assert.Equal(t, 7, ans)
 }
