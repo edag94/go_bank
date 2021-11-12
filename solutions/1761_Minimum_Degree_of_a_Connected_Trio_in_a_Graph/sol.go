a03f28a24692f884089cb8584312e337b0332e03
edag94 <edag94@live.com>
2021-11-12 18:30:11 -0500

    don't need visited actually. But need to walk diagonal

--- solutions/1761_Minimum_Degree_of_a_Connected_Trio_in_a_Graph/sol.go
+++ solutions/1761_Minimum_Degree_of_a_Connected_Trio_in_a_Graph/sol.go
@@ -4,9 +4,16 @@ func minTrioDegree(n int, edges [][]int) int {
 	// build adjacency matrix and visited
 
 	adjacencyMatrix := make([][]int, n)
+	visitedMatrix := make([][]int, n)
+
 	for i := range adjacencyMatrix {
 		for j := 0; j < n; j++ {
 			adjacencyMatrix[i] = make([]int, n)
+			visitedMatrix[i] = make([]int, n)
+			for k := 0; k < n; k++ {
+				adjacencyMatrix[i][k] = 0
+				visitedMatrix[i][k] = 0
+			}
 		}
 	}
 
@@ -16,5 +23,5 @@ func minTrioDegree(n int, edges [][]int) int {
 		adjacencyMatrix[x][y] = 1
 	}
 
-	// walk the matrix. If
+	// walk the matrix. If a 1, you have 2 edges, so check 3rd for a trio. Check all other nodes to see if 1s.
 }
