c0093ffbe6b998964820f9f4841fd2f08eb0c823
edag94 <edag94@live.com>
2021-11-12 23:19:32 -0500

    to revisit

--- solutions/1761_Minimum_Degree_of_a_Connected_Trio_in_a_Graph/sol_test.go
+++ solutions/1761_Minimum_Degree_of_a_Connected_Trio_in_a_Graph/sol_test.go
@@ -2,8 +2,13 @@ package Minimum_Degree_of_a_Connected_Trio_in_a_Graph
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test(t *testing.T) {
-
+	n := 6
+	edges := [][]int{[]int{1, 2}, []int{1, 3}, []int{3, 2}, []int{4, 1}, []int{5, 2}, []int{3, 6}}
+	sol := minTrioDegree(n, edges)
+	assert.Equal(t, 3, sol)
 }
