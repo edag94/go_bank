5daeba23cd918537b5610d762f915244b744f878
edag94 <edag94@live.com>
2021-11-20 00:53:32 -0500

    524 (#4)
    * First attempt
    
    * add comment on how to make better

---
+++ solutions/524_Longest_Word_in_Dictionary_through_Deleting/sol.go
@@ -0,0 +1,38 @@
+package Longest_Word_in_Dictionary_through_Deleting
+
+// can improve on this by sorting dictionary by len desc
+func findLongestWord(s string, dictionary []string) string {
+	sol := ""
+	for i := range dictionary {
+		word := dictionary[i]
+		end := len(word)
+		wordIndex := 0
+		for sIndex := range s {
+			sChar := string(s[sIndex])
+			wChar := string(word[wordIndex])
+			if sChar == wChar {
+				wordIndex++
+			}
+			if wordIndex >= end {
+				// is solution
+
+				if len(word) > len(sol) {
+					sol = word
+				} else if len(word) == len(sol) {
+					if isNewSmaller(sol, word) {
+						sol = word
+					}
+				}
+				break
+			}
+		}
+	}
+	return sol
+}
+
+func isNewSmaller(first string, second string) bool {
+	if first == "" {
+		return true
+	}
+	return second < first
+}
